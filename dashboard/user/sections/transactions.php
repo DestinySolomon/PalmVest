9f1d5db469a758f669c5084af260dc9bcdb83e49
DestinySolomon <deesystems247@gmail.com>
2025-11-08 16:02:55 +0100

    made a better path for transactions file

--- dashboard/user/sections/transactions.php
+++ dashboard/user/sections/transactions.php
@@ -1,7 +1,6 @@
 <?php
-require_once __DIR__ . '/../../config/db.php';
+require_once __DIR__ .'/../../../config/db.php';
 require_once __DIR__ . '/../helpers.php';
-
 $user_id = $_SESSION['user_id'];
 
 // Fetch all transactions for the user
